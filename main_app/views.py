deb7063228a692abe62e5790e942cb21e245e6ab
wkriegsman <wkriegsman@gmail.com>
2020-10-30 16:52:49 -0400

    creates batters and pitchers views

--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def batters(request):
+    return render(request, 'batters.html')
+
+def pitchers(request):
+    return render(request, 'pitchers.html')    
