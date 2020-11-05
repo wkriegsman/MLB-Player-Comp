ffb8287d6399f2a206ad7691ec99c90289e56e47
wkriegsman <wkriegsman@gmail.com>
2020-11-05 10:15:23 -0500

    creates view for chart and data

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+from .views import get_data
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('batters/', views.batters, name='batters'),
     path('pitchers/', views.pitchers, name='pitchers'),
+    path('api/data/', get_data, name='api-data')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
@@ -9,4 +10,24 @@ def batters(request):
     return render(request, 'batters.html')
 
 def pitchers(request):
-    return render(request, 'pitchers.html')    
+    return render(request, 'pitchers.html')   
+
+
+def get_data(request, *args, **kwargs):
+    data = {
+    "ab": 470,
+    "avg": .291,
+    "babip": .298,
+    "h": 137,
+    "hr": 45,
+    "obp": .438,
+    "ops": 1.083,
+    "r": 110,
+    "rbi": 104,
+    "sb": 11,
+    "slg": .645,
+    "so": 11,
+    "vorp": 81.5,
+    "warp": 9.34,
+    }
+    return JsonResponse(data)
