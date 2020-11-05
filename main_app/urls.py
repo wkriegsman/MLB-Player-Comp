eebb0c6d4dcfa2853c80c004391b187c94884da5
wkriegsman <wkriegsman@gmail.com>
2020-11-05 11:32:19 -0500

    adds chart views and utilizes djangorestframework

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-from .views import get_data
+from .views import get_data, ChartData
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('batters/', views.batters, name='batters'),
     path('pitchers/', views.pitchers, name='pitchers'),
-    path('api/data/', get_data, name='api-data')
+    path('api/data/', get_data, name='api-data'),
+    path('api/chart/data/', ChartData.as_view()),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 def home(request):
@@ -31,3 +33,30 @@ def get_data(request, *args, **kwargs):
     "warp": 9.34,
     }
     return JsonResponse(data)
+
+
+class ChartData(APIView):
+    
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+        data = {
+            "ab": 470,
+            "avg": .291,
+            "babip": .298,
+            "h": 137,
+            "hr": 45,
+            "obp": .438,
+            "ops": 1.083,
+            "r": 110,
+            "rbi": 104,
+            "sb": 11,
+            "slg": .645,
+            "so": 11,
+            "vorp": 81.5,
+            "warp": 9.34,
+    }
+        return Response(data)
+
+        
